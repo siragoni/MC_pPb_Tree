cb6a9b5d08d0dabbfed9c8b6665b0fff410c3391
Simone Ragoni <simone.ragoni@cern.ch>
2020-12-01 07:44:06 +0000

    Gamma ana.

--- runAnalysis.C
+++ runAnalysis.C
@@ -257,8 +257,8 @@ void runAnalysis(Int_t period)
             mgr->StartAnalysis("grid");
         } else {
 	  // else launch the full grid analysis
-	  alienHandler->SetRunMode("full");
-	  // alienHandler->SetRunMode("terminate");
+	  // alienHandler->SetRunMode("full");
+	  alienHandler->SetRunMode("terminate");
 	  mgr->StartAnalysis("grid");
         }
     }
